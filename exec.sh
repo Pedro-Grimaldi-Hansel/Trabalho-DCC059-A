2ed3b5dead9275aaf251071edb69416aaa24bd23
Pedro Leopoldo <79977884+Leopalds@users.noreply.github.com>
2023-06-18 14:29:22 -0300

    Criamos um script para executar todas as instancias

---
+++ exec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Compilar o código C++
+cd build
+make
+
+# Pasta de entrada
+input_dir="instancias"
+
+# Arquivo de saída
+output_file="arquivoDeSaida.txt"
+
+# Executar o programa para cada arquivo de entrada
+for input_file in $input_dir/*
+do
+    echo $input_file
+    /Users/pedroleopoldo/Documents/Estudos/Trabalho-DCC059-A/build/main $input_file $output_file 0 0 1
+done
